442ccf4169d6286fcad0e57eac21d352cc11bacc
karine-555 <karineatoyan44@gmail.com>
2023-04-25 10:33:59 -0800

    added loops

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 "use strict";
-
-const numberOfFilms = +prompt(
+let numberOfFilms = +prompt(
   "Այսօր քանի՞ ֆիլմ եք դիտել",
   "Խնդրում ենք լրացնել թվերով"
 );
-
+while (numberOfFilms === "" || numberOfFilms === null || isNaN(numberOfFilms)) {
+  let numberOfFilms = +prompt(
+    "Այսօր քանի՞ ֆիլմ եք դիտել",
+    "Խնդրում ենք լրացնել թվերով"
+  );}
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: isNaN(numberOfFilms) ? 0 : numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: false,
-};
-
-const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", ""),
-      b = +prompt("Ինչքա՞ն եք գնահատում այդ ֆիլմը"),
-      c = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", ""),
-      d = +prompt("Ինչքա՞ն եք գնահատում այդ ֆիլմը");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+  privat: true,};
+for (let i = 1; i <= numberOfFilms; i++) {
+  let whichFilm = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", "");
+  while (whichFilm === "" || whichFilm === null) {
+    whichFilm = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", "");}
+  let rateFilm = +prompt(`Ինչքա՞ն եք գնահատում <<${whichFilm}>> ֆիլմը`);
+  while (rateFilm === "" || rateFilm === null || isNaN(rateFilm)) {
+    rateFilm = +prompt(`Ինչքա՞ն եք գնահատում <<${whichFilm}>> ֆիլմը`);}
+  personalMovieDB.movies[whichFilm] = rateFilm;
+}
+if( personalMovieDB.privat === false) {
+  console.log("The database is closed")
+}else {
+  console.log(personalMovieDB);
+}
 
